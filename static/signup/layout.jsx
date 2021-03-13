e7b06e2c2935f981e0f9e2cbfbea4116da194381
moonxibot <xibotmoon@gmail.com>
2021-03-13 22:34:07 +0900

    회원가입 페이지 리뉴얼에 따른 레이아웃 담당 JSX 추가

--- static/signup/layout.jsx
+++ static/signup/layout.jsx
@@ -1,17 +1,10 @@
-function next(id) {
-    if (document.getElementById(`input${id}`).value.trim() !== "") {
-        document.getElementById("div" + id).hidden = true;
-        document.getElementById("div" + (Number(id) + 1)).hidden = false;
-    }
-}
-
 class Index extends React.Component {
     render() {
         return (
-            <div style={{textAlign: "center"}} id={"div" + this.props.divid} hidden>
-                <h1 style={{fontSize: "2rem", marginBottom: "1vh"}}>{this.props.title}</h1>
-                <input type="text" placeholder={this.props.placeholder} style={{marginBottom: "1vh"}} id={"input" + this.props.divid} type={this.props.type} /><br />
-                <button id={this.props.divid} onClick={() => {next(this.props.divid)}}>확인</button>
+            <div style={{textAlign: "center"}} id={"div" + this.props.idname} hidden>
+                <h1 style={{fontSize: "2rem", marginBottom: "1.5vh"}}>{this.props.title}</h1>
+                <input type="text" placeholder={this.props.placeholder} style={{marginBottom: "1vh"}} id={"input" + this.props.idname} type={this.props.type} /><br />
+                <button id={this.props.idname} onClick={() => {next(this.props.idname)}}>확인</button>
             </div>
         )
     }
@@ -19,9 +12,10 @@ class Index extends React.Component {
 
 ReactDOM.render(
     <div>
-        <Index title="계정 이메일" placeholder="이메일" divid="1" type="email" />
-        <Index title="비밀번호" placeholder="비밀번호" divid="2" type="password" />
-        <Index title="비밀번호" placeholder="다시 비밀번호를 입력하세요." divid="3" type="password" />
+        <Index title="이메일" placeholder="이메일 입력..." idname="1" type="email" />
+        <Index title="닉네임" placeholder="실명은 입력하지 마세요." idname="2" type="email" />
+        <Index title="비밀번호" placeholder="비밀번호..." idname="3" type="password" />
+        <Index title="비밀번호" placeholder="다시 비밀번호를 입력하세요..." idname="4" type="password" />
     </div>
 , document.getElementById("root"))
 
