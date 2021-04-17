51a86087da56c992520692bc4bae85decb03f4ae
xibotmoon <xibotmoon@gmail.com>
2021-04-17 14:24:32 +0900

    레이어 팝업 추가

--- static/api.js
+++ static/api.js
@@ -1,1 +1,9 @@
-const api = {}
+const popup = {
+    open: (value) => {
+        document.getElementById("pop").hidden = false;
+        document.getElementById("popvalue").innerHTML = value;
+    },
+    close: () => {
+        document.getElementById("pop").hidden = true;
+    }
+}
