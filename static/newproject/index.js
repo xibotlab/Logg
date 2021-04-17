7d90d58b67a5b7b20fe8106fc6ba6c146c7cb47e
xibotmoon <xibotmoon@gmail.com>
2021-04-17 14:25:15 +0900

    버튼 클릭 시 Ajax 이벤트 추가

--- static/newproject/index.js
+++ static/newproject/index.js
@@ -1,6 +1,18 @@
-new Vue({
-    el: "#reco",
-    data: {
-        value: `#${String(Math.random()).substring(2, 7)}`
+document.getElementById("submit").addEventListener("click", () => {
+    //element 가져오기
+    const name = document.getElementById("name").value;
+    const desc = document.getElementById("desc").value;
+
+    //공백 확인
+    if (name.trim() == "" || desc.trim() == "") {
+        popup.open("프로젝트 이름과 설명은 필수입니다.<br>올바른 값을 입력해주세요.")
+    } else {
+        fetch("/api/new/", {
+            method: "POST",
+            body: JSON.stringify({
+                name: name,
+                desc: desc
+            })
+        })
     }
 })
